1abf0e4b18ea75b8f3ea22c7669d1ff898acd045
Pheak Rattana <rattana.pheak.rp@gmail.com>
2023-03-14 15:34:33 +0100

    Update Nav css
    Update Nav css

--- meal-db-v3/src/views/HeaderView.vue
+++ meal-db-v3/src/views/HeaderView.vue
@@ -1,23 +1,49 @@
 <template>
-  <header>  
-    <nav class="d-flex">
+  <header>
+    <nav class="d-flex align-items-center mb-20">
+      <figure>
+        <img src="@/assets/images/logo.jpg" alt="logo">
+      </figure>
       <ul class="d-flex">
-        <li><router-link to="/">Home</router-link></li>
-        <li><router-link to="/categories">Categories</router-link></li>
-        <li><router-link to="/areas">Areas</router-link></li>
-        <li><router-link to="/meals">Meals</router-link></li>
+        <router-link to="/">Home</router-link>
+        <router-link to="/categories">Categories</router-link>
+        <router-link to="/areas">Areas</router-link>
+        <router-link to="/meals">Meals</router-link>
       </ul>
     </nav>
   </header>
 </template>
 
-
 <script setup lang="ts">
 </script>
 
 <style scoped lang="scss">
+@import '@/assets/style/main.scss';
 
-li {
-  margin: 10px;
+header {
+  background-color: var(--primary-2);
+
+  nav {
+    justify-content: space-around;
+
+    figure {
+      margin: 0;
+      
+      img {
+        vertical-align: middle;
+        width: 100px;
+        height: 100px;
+      }
+    }
+    
+    a {
+      text-decoration: none;
+      margin: 10px;
+      color: white;
+      &:hover {
+        color: var(--gray-2);
+      }
+    }
+  }
 }
 </style>
