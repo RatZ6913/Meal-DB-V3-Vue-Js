877d6af5f558dd717d6aeb789c83d2e886e36b35
Pheak Rattana <rattana.pheak.rp@gmail.com>
2023-03-14 15:46:05 +0100

    Update Base Pinia
    Update Base Pinia

--- meal-db-v3/src/main.ts
+++ meal-db-v3/src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import { createRouter, createWebHistory } from 'vue-router';
 import { routes } from './router/routes';
 import App from './App.vue';
@@ -8,7 +9,9 @@ const router = createRouter({
   routes: routes,
 })
 
+const pinia = createPinia();
 const app = createApp(App);
-app.use(router);
-app.mount('#app');
 
+app.use(router);
+app.use(pinia);
+app.mount('#app');
