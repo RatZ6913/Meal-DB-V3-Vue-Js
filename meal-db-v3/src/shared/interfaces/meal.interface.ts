b9fe7c04591885a5e55ed845d5114b2c740e23f5
Pheak Rattana <rattana.pheak.rp@gmail.com>
2023-03-14 15:59:45 +0100

    Update Meal Interface
    Update Meal Interface

---
+++ meal-db-v3/src/shared/interfaces/meal.interface.ts
@@ -0,0 +1,11 @@
+export interface Meal {
+  idMeal: string;
+  strMeal: string;
+  strArea: string;
+  categories: string;
+  strMealThumb: string;
+  strYoutube: string;
+  strIngredient: [];
+  strMeasure: [];
+  strInstructions: string;
+}
